06cee453226b1bd57f848be964bb832648aa76e6
adriana-codes <helloadrianadiaz@gmail.com>
2025-03-20 13:34:44 +0100

    +NavBar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import "./index.css";
 import "./App.css";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello from React 🙋🏻‍♀️ <br /> Setting things up to get ready to build! 💪
-        </p>
+        <NavBar />
       </header>
+      <footer>
+        {" "}
+        <p>Adri Díaz -- 2025 ©</p>{" "}
+      </footer>
     </div>
   );
 }
