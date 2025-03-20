d2d5139e00bf9906e869a0e10e40439279fcc369
adriana-codes <helloadrianadiaz@gmail.com>
2025-03-20 12:06:17 +0100

    +Initial commit
    Adrifolio creation - added React - Bootstrap - fonts to be used

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello from React 🙋🏻‍♀️ <br /> Setting things up to get ready to build! 💪
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
