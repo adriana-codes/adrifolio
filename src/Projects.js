855a0d13139ea9596eb5b908120c4c3fe0cb1b33
adriana-codes <helloadrianadiaz@gmail.com>
2025-03-24 11:31:43 +0100

    Update Projects.js

--- src/Projects.js
+++ src/Projects.js
@@ -72,8 +72,12 @@ function Projects() {
         <div className="Project-block">
           <h4 className="Project-title">The Cocktail Maker</h4>
           <p className="Project-description">
-            Motivated by my time in the FMCG leader Pernod Ricard. I created
-            this cocktail recipe maker with the help of an AI integration.
+            Motivated by my passion for the wine and spirit's industry. I
+            created this cocktail recipe maker with the help of an AI
+            integration.
+            <br />
+            You can input any ingredient and you'll receive a recipe based on
+            it. Try it!
           </p>
           <h5>Tools</h5>
           <div className="Tools-section">
@@ -99,18 +103,15 @@ function Projects() {
         <div className="Project-block">
           <h4 className="Project-title">World Clock</h4>
           <p className="Project-description">
-            This weather app was built using React with an API integration. It
-            includes a search engine to search any city, and will give you
-            details on humidity, wind speed, a description of the weather, and a
-            five day forecast.
+            I built this world clock app using JS timing events, date
+            formatting, and timezones.
           </p>
           <h5>Tools</h5>
           <div className="Tools-section">
-            <span className="Tools-btn">API</span>
+            <span className="Tools-btn">Javascript</span>
             <span className="Tools-btn">CSS</span>
             <span className="Tools-btn">Github</span>
             <span className="Tools-btn">HTML</span>
-            <span className="Tools-btn">React.js</span>
           </div>
           <div>
             <a
