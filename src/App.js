c6dfcaa6221d5967d2cbc6e2d24afcab7dfd1bac
adriana-codes <helloadrianadiaz@gmail.com>
2025-03-24 12:50:12 +0100

    +Contact +favicon fix & styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FooterInfo from "./FooterInfo";
 import Hero from "./Hero";
 import TechSkills from "./TechSkills";
 import Projects from "./Projects";
+import Contact from "./Contact";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Hero />
         <TechSkills />
         <Projects />
+        <Contact />
       </main>
       <hr />
       <footer>
---
+++ src/Contact.js
@@ -0,0 +1,32 @@
+import "./Contact.css";
+import dogmail from "./images/dog-mail-removebg-preview.png";
+
+function Contact() {
+  return (
+    <div className="Contact" id="contact">
+      <div className="grid">
+        <div className="Contactfo">
+          <h1>Wanna say hey?</h1>
+          <div>
+            <h4>
+              Pop me an email at 💌
+              <a href="mailto:helloadrianadiaz@gmail.com" class="email-link">
+                <strong>helloadrianadiaz@gmail.com</strong>
+              </a>
+              -
+              <br />
+              I'd love to hear from you!
+            </h4>
+
+            <p>📍 Based in Barcelona, SP and the Internet</p>
+          </div>
+        </div>
+        <div>
+          <img src={dogmail} alt="Email me"></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
